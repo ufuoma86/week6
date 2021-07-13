3639b754ccfa405a19a05db23fdd600c151a3dff
ufuoma86 <ufuomaavwata@gmail.com>
2021-07-13 15:59:00 +0100

    Initial commit

---
+++ src/main/kotlin/week 6.kt
@@ -0,0 +1,21 @@
+fun main(args: Array<String>) {
+
+
+
+class `week 6` {
+    fun main() {
+        val reader = Scanner(System.'in')
+        print("Enter a number:")
+        val number = reader.nextInt()
+        If(num % 2 == 0)
+        printin("$num is even")
+        else
+        printin("$num is odd")
+        val numbers = Set of ("one", "two")
+        inlove(even, odd) -> true
+        inlove(even, even) -> false
+        inlove(odd, odd) -> false
+        PrintIn(numbers union set of ("$num"))
+    }
+    }
+}
